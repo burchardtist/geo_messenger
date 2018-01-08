e0d8c187d1d98eef80a663ddffcbe7571e6eaa0d
Aleksander Philips <aleksander.philips@venturedevs.co>
2018-01-08 09:26:51 +0100

    added lat and lon for messages

--- client/GeoMessenger/components/addMessage.js
+++ client/GeoMessenger/components/addMessage.js
@@ -8,9 +8,61 @@ const MessageForm = t.form.Form;
 const Message = t.struct({
   title: t.String,
   text: t.String,
+  life_time: t.maybe(t.Number),
+  range: t.maybe(t.Number),
+  lat: t.maybe(t.Number),
+  lon: t.maybe(t.Number),
 });
 
+const formStyles = {
+  ...MessageForm.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+};
+
+const options = {
+  fields: {
+    title: {
+      error: 'Title is required',
+    },
+    text: {
+      error: 'Text is required',
+    },
+    lat: {
+      hidden: true,
+    },
+    lon: {
+      hidden: true,
+    }
+  },
+  stylesheet: formStyles,
+};
+
+
 class AddMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: {},
+    };
+  }
+
   static navigationOptions = {
     title: 'Add new message',
   };
@@ -20,6 +72,7 @@ class AddMessage extends Component {
     if (!formValues){
       return;
     }
+
     MessagesApi.addMessage(formValues)
       .then((responseJson) => {
         if(responseJson) {
@@ -28,12 +81,29 @@ class AddMessage extends Component {
       })
   };
 
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        this.setState({
+          value: {
+            lat: coords.latitude,
+            lon: coords.longitude,
+          }
+        });
+      },
+      (error) => console.error(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MessageForm
           ref={c => this._form = c}
           type={Message}
+          options={options}
+          value={this.state.value}
         />
         <Button
           title="Add"
