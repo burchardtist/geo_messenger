662a93f7fb278dd9de8c2fce0074b79be54e412e
Aleksander Philips <aleksander.philips@venturedevs.co>
2018-01-21 23:22:24 +0100

    added distance of msg

--- client/GeoMessenger/api/api.js
+++ client/GeoMessenger/api/api.js
@@ -35,7 +35,7 @@ const MessagesApi = {
           )
         },
         (error) => console.error(error.message),
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        { enableHighAccuracy: true, timeout: 10000 }
       )
     });
   },
--- client/GeoMessenger/components/message.js
+++ client/GeoMessenger/components/message.js
@@ -3,11 +3,17 @@ import { Text, View, StyleSheet } from "react-native";
 import { List, ListItem } from "react-native-elements";
 
 const Message = ({item}) => {
-  const {title, text, lat, lon} = item;
+  const {title, text, distance} = item;
+  let parsedDistance;
+  if(distance <= 1) {
+    parsedDistance = `${Math.round(distance*1000)} meters`;
+  } else {
+    parsedDistance = `${distance.toFixed(3)}km`;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
-      <Text style={styles.location}>lat: {lat} lon: {lon}</Text>
+      <Text style={styles.location}>distance: { parsedDistance }</Text>
       <Text style={styles.text}>{text}</Text>
     </View>
   )
@@ -17,7 +23,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#DDD',
     paddingTop: 10,
-
   },
   title: {
     textAlign: 'center',
